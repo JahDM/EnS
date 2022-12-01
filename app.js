669d7a5711af6e429da77a14884ed384df893849
JahDM <hubertwatycha20013112@gmail.com>
2022-12-01 12:37:35 +0100

    colouring divs

--- app.js
+++ app.js
@@ -7,15 +7,23 @@ for (let i = 0; i < 256; i++) {
     bigContainer.appendChild(box);
 }
 */
+
+
+
 const bigContainer = document.getElementById('bigContainer');
+let box;
+
 function makeRows(rows, cols) {
   bigContainer.style.setProperty('--grid-rows', rows);
   bigContainer.style.setProperty('--grid-cols', cols);
   for (i = 0; i < rows * cols; i++) {
     let box = document.createElement('div');
     box.classList.add('box');
-    box.textContent = (i+1);
     bigContainer.appendChild(box);
+    document.getElementsByClassName('box')
+    box.addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'black';
+})
   }
 }
 makeRows(16, 16);
