127d46389d4a5e7e65201c59ff373e27a6ee2425
JahDM <hubertwatycha20013112@gmail.com>
2022-12-03 12:00:58 +0100

    grid done

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-
 const bigContainer = document.getElementById('bigContainer');
-let box;
+const cancelButton = document.getElementById('cancelButton');
 
 function makeRows(rows, cols) {
   bigContainer.style.setProperty('--grid-rows', rows);
@@ -10,39 +9,43 @@ function makeRows(rows, cols) {
     box.classList.add('box');
     bigContainer.appendChild(box);
     box.addEventListener('mouseover', (event) => {
-    event.target.style.backgroundColor = '#f5f5f5';
-})
+      event.target.style.backgroundColor = 'pink';
+    });
+function reset() {
+      while (bigContainer.firstChild) {
+        bigContainer.removeChild(bigContainer.firstChild);
+      }
+    }
+cancelButton.addEventListener('click', () => {
+      reset();
+    });
+
   }
 }
-//makeRows(16, 16);
-let grid4X4  = document.getElementById('sixTeen');
-sixTeen.addEventListener('click', () => {
 
+//makeRows(16, 16);
+let grid4X4 = document.getElementById('sixTeen');
+sixTeen.addEventListener('click', () => {
   makeRows(4, 4);
 });
-let grid8X8  = document.getElementById('sixtyFour');
+let grid8X8 = document.getElementById('sixtyFour');
 sixtyFour.addEventListener('click', () => {
-
   makeRows(8, 8);
 });
-let grid12X12  = document.getElementById('hundredFortyFour');
+let grid12X12 = document.getElementById('hundredFortyFour');
 hundredFortyFour.addEventListener('click', () => {
-
   makeRows(12, 12);
 });
-let grid16X16  = document.getElementById('twoHundredFiftySix');
+let grid16X16 = document.getElementById('twoHundredFiftySix');
 twoHundredFiftySix.addEventListener('click', () => {
-
   makeRows(16, 16);
 });
-let grid32X32  = document.getElementById('oneKTwentyFour');
+let grid32X32 = document.getElementById('oneKTwentyFour');
 oneKTwentyFour.addEventListener('click', () => {
-
   makeRows(32, 32);
 });
-let grid64X64  = document.getElementById('fourKninetySix');
+let grid64X64 = document.getElementById('fourKninetySix');
 fourKninetySix.addEventListener('click', () => {
-
   makeRows(32, 32);
 });
 
